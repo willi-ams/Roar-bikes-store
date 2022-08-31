87f08c3eaac57c7f6a092625b37ab69c7459a3a5
williams <williams.bolu99@gmail.com>
2022-08-31 12:57:21 -0700

    test

--- src/App.js
+++ src/App.js
@@ -5,21 +5,29 @@ import ScrollToTop from "./ScrollToTop";
 import LoadingSpinner from "./components/layout/LoadingSpinner";
 import Layout from "./components/layout/Layout";
 
-const Home = React.lazy(() => import('./pages/Home'));
-const RoadBikes = React.lazy(() => import('./pages/RoadBikes'));
-const MountainBikes = React.lazy(() => import('./pages/MountainBikes'));
-const FoldingBikes = React.lazy(() => import('./pages/FoldingBikes'));
-const ProductDetail = React.lazy(() => import('./pages/ProductDetail'));
-const NotFound = React.lazy(() => import('./pages/NotFound'));
-const CartLink = React.lazy(() => import('./pages/CartLink'));
+// const Home = React.lazy(() => import('./pages/Home'));
+// const RoadBikes = React.lazy(() => import('./pages/RoadBikes'));
+// const MountainBikes = React.lazy(() => import('./pages/MountainBikes'));
+// const FoldingBikes = React.lazy(() => import('./pages/FoldingBikes'));
+// const ProductDetail = React.lazy(() => import('./pages/ProductDetail'));
+// const NotFound = React.lazy(() => import('./pages/NotFound'));
+// const CartLink = React.lazy(() => import('./pages/CartLink'));
 // import Contact from './pages/Contact';
+
+import Home from "./pages/Home";
+import RoadBikes from "./pages/RoadBikes";
+import MountainBikes from "./pages/MountainBikes";
+import FoldingBikes from "./pages/FoldingBikes";
+import ProductDetail from "./pages/ProductDetail";
+import NotFound from "./pages/NotFound";
+import CartLink from "./pages/CartLink";
 
 function App() {
 
   return (
     <ScrollToTop>
       <Layout>
-        <Suspense fallback={<div className="centered"><LoadingSpinner /></div>}>
+        {/* <Suspense fallback={<div className="centered"><LoadingSpinner /></div>}> */}
           <Routes>
             <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Home />} />
@@ -34,7 +42,7 @@ function App() {
             {/* <Route path="/contact" element={<Contact />} /> */}
 
           </Routes>
-        </Suspense>
+        {/* </Suspense> */}
       </Layout>
     </ScrollToTop>
   );
